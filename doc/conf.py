23fff41eb70cb83b03afd67c9075f757ce9e0d0b
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2016-06-20 10:16:22 +0200

    [doc] remove the imgmath package from sphinx (seems to prevent CI builds on gitlab)

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,6 @@ import pkg_resources
 extensions = [
   'sphinx.ext.todo',
   'sphinx.ext.coverage',
-  'sphinx.ext.imgmath',
   'sphinx.ext.ifconfig',
   'sphinx.ext.autodoc',
   'sphinx.ext.autosummary',
