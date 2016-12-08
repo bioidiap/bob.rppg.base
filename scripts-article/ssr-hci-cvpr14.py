99e91c802f900e85284156648cf3df8e080d113f
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2016-12-08 22:53:50 +0100

    [scripts] added scripts to generate results in the paper

---
+++ scripts-article/ssr-hci-cvpr14.py
@@ -0,0 +1,56 @@
+import os, sys
+from gridtk.sge import JobManagerSGE
+
+# directories and file
+base_expe_dir = 'experiments/paper/ssr-hci-cvpr14/'
+pulse_dir = base_expe_dir + 'pulse'
+hr_dir = base_expe_dir + 'hr'
+results_dir = base_expe_dir + 'results'
+
+number_of_jobs = 527 
+framerate = 61
+
+# parameters
+skin_threshold = 0.1
+stride = 30 
+
+n_segments = 8
+nfft = 4096
+
+# write a file with the parameters - useful to keep track sometimes ..
+param_file = base_expe_dir + '/parameters.txt'
+if not os.path.isdir(base_expe_dir):
+  os.mkdir(base_expe_dir)
+
+f = open(param_file, 'w')
+f.write('skin threshold = ' + str(skin_threshold) + '\n\n')
+f.write('stride = ' + str(stride) + '\n\n')
+f.write('Welch segments = ' + str(n_segments) + '\n')
+f.write('npoints FFT = ' + str(nfft) + '\n')
+f.close()
+
+# check job status on the grid
+def check_job_status(job_id):
+  manager.lock()
+  job = manager.get_jobs([job_id])
+  manager.unlock()
+  return str(job[0].status)
+
+# pulse extraction
+manager = JobManagerSGE(database='submitted.sql3', wrapper_script=os.path.abspath('./bin/jman'))
+extract = ['./bin/ssr_pulse.py', 'hci', '--protocol', 'cvpr14', '--dbdir', 'hci', '--bboxdir', 'bounding-boxes', '--outdir', str(pulse_dir), '--threshold', str(skin_threshold), '--stride', str(stride), '--start', '306', '--end', '2136']
+job_id = manager.submit(extract, array=(1, number_of_jobs, 1))
+print 'Running job {0} (extraction) on the grid ...'.format(job_id)
+
+# while the job is not succesfull, just wait
+job_status = check_job_status(job_id)
+while (job_status != 'success'):
+  job_status = check_job_status(job_id)
+
+os.system('./bin/ssr_pulse.py hci --protocol cvpr14 --dbdir hci --bboxdir bounding-boxes --outdir ' + str(pulse_dir) + ' --threshold ' + str(skin_threshold) + ' --stride ' + str(stride) + ' --start 306 --end 2136 -v --skininit')
+
+# computing heart-rate
+os.system('./bin/rppg_get_heart_rate.py hci --protocol cvpr14 --indir ' + pulse_dir + ' --outdir ' + hr_dir + ' --framerate ' + str(framerate) + ' --nsegments ' +str(n_segments) + ' --nfft ' + str(nfft) + ' -v')
+
+# computing performance
+os.system('./bin/rppg_compute_performance.py  hci --protocol cvpr14 --indir ' + hr_dir + ' --outdir ' + results_dir + ' -v')
