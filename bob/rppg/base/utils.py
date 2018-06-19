c3b29f61c87ef073a745aed3a594a2c2a393f5bc
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2018-06-19 14:07:19 +0200

    [base utils] docstrings in numpy formal

--- bob/rppg/base/utils.py
+++ bob/rppg/base/utils.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Copyright (c) 2017 Idiap Research Institute, http://www.idiap.ch/
-# Written by Guillaume Heusch <guillaume.heusch@idiap.ch>,
-# 
-# This file is part of bob.rpgg.base.
-# 
-# bob.rppg.base is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
-# published by the Free Software Foundation.
-# 
-# bob.rppg.base is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with bob.rppg.base. If not, see <http://www.gnu.org/licenses/>.
-
-
 import os, sys
 import numpy
 import collections
@@ -27,25 +9,22 @@ import bob.ip.base
 import bob.ip.facedetect
 
 def scale_image(image, width, height):
-  """scale_image(image, width, height) -> scaled_image
-  
-  This function scales an image.
+  """scales an image.
 
-  **Parameters**
-  
-    ``image`` : (3d numpy array)
-      The image to be scaled.
-  
-    ``width`` : (int)
-      The new image width.
-  
-    ``height``: (int)
-      The new image height
+  Parameters
+  ----------
+  image: 3d numpy array
+    The image to be scaled.
+  width: int
+    The new image width.
+  height: int
+    The new image height
 
-  **Returns**
+  Returns
+  -------
+  result: 3d numpy array
+    The scaled image
   
-    ``result`` : (3d numpy array)
-      The scaled image
   """
   assert len(image.shape) == 3, "This is meant to work with color images (3 channels)"
   result = numpy.zeros((3, width, height))
@@ -54,25 +33,21 @@ def scale_image(image, width, height):
 
 
 def crop_face(image, bbx, facewidth):
-  """crop_face(image, bbx, facewidth) -> face
+  """crops a face from an image.
   
-  This function crops a face from an image.
-  
-  **Parameters**
-  
-    ``image`` : (3d numpy array )
-      The image containing the face.
+  Parameters
+  ----------
+  image: 3d numpy array
+    The image to be scaled.
+  bbx: :py:class:`bob.ip.facedetect.BoundingBox`
+    The bounding box of the face.
+  facewidth: int
+    The width of the face after cropping.
 
-    ``bbx`` : (bob.ip.facedetect.BoundingBox)
-      The bounding box of the face.
-
-    ``facewidth``: (int)
-      The width of the face after cropping.
-
-  **Returns**
-    
-    ``face`` : (numpy array)
-      The face image.
+  Returns
+  -------
+  face: numpy array
+    The face image.
   """
   face = image[:, bbx.topleft[0]:(bbx.topleft[0] + bbx.size[0]), bbx.topleft[1]:(bbx.topleft[1] + bbx.size[1])]
   aspect_ratio = bbx.size_f[0] / bbx.size_f[1] # height/width
@@ -83,29 +58,24 @@ def crop_face(image, bbx, facewidth):
 
 
 def build_bandpass_filter(fs, order, plot=False):
-  """build_bandpass_filter(fs, order[, plot]) -> b
+  """builds a butterworth bandpass filter.
   
-  Builds a butterworth bandpass filter.
+  Parameters
+  ----------
+  fs: float
+    sampling frequency of the signal (i.e. framerate).
+  order: int
+    The order of the filter (the higher, the sharper).
+  plot: bool
+    Plots the frequency response of the filter.
   
-  **Parameters**
-
-    ``fs`` : (float)
-      sampling frequency of the signal (i.e. framerate).
-    
-    ``order`` : (int)
-      The order of the filter (the higher, the sharper).
+  Returns
+  -------
+  b: numpy array
+    The coefficients of the FIR filter.
   
-    ``plot`` : ([Optional] boolean)
-      Plots the frequency response of the filter.
-      Defaults to False.
-  
-  **Returns**
-    
-    ``b`` : (numpy array)
-      The coefficients of the FIR filter.
   """
-  # frequency range in Hertz, corresponds to plausible h
-  #heart-rate values, i.e. [42-240] beats per minute
+  # frequency range in Hertz, corresponds to plausible heart-rate values, i.e. [42-240] beats per minute
   min_freq = 0.7 
   max_freq = 4.0 
 
