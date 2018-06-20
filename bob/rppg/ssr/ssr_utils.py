8269656fe967205123268483151316ff0ed5aac2
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2018-06-20 15:06:24 +0200

    [ssr] fixed docstring in utils

--- bob/rppg/ssr/ssr_utils.py
+++ bob/rppg/ssr/ssr_utils.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Copyright (c) 2017 Idiap Research Institute, http://www.idiap.ch/
-# Written by Guillaume Heusch <guillaume.heusch@idiap.ch>,
-# 
-# This file is part of bob.rpgg.base.
-# 
-# bob.rppg.base is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
-# published by the Free Software Foundation.
-# 
-# bob.rppg.base is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with bob.rppg.base. If not, see <http://www.gnu.org/licenses/>.
-
 import numpy
 import bob.ip.base
 from ..base.utils import crop_face
@@ -26,38 +9,37 @@ from bob.ip.skincolorfilter import SkinColorFilter
 skin_filter = SkinColorFilter()
 
 def get_skin_pixels(face_frame, index, skininit, threshold, bounding_boxes=None, skin_frame=None, plot=False):
-  """get_skin_pixels(face_frame, index, facewidth, skininit, threshold, bounding_boxes=None, skin_frame=None, plot=False) -> skin_pixels
- 
-    Get a list of skin colored pixels inside the given frame.
+  """get a list of skin colored pixels inside the given frame.
     
-    **Parameters**
+  Parameters
+  ----------
+  face_frame: numpy ndarray
+    The frame where the face has to be detected.
 
-    ``face_frame`` (numpy array):
-      The frame where the face has to be detected.
+  index: int
+    The index of the frame containing the face to be detected.
 
-    ``index`` (int):
-      The index of the frame containing the face to be detected.
+  skininit: bool
+    Flag if you want the parameters of the skin model to be re-estimated.
 
-    ``skininit`` (boolean):
-      Flag if you want the parameters of the skin model to be re-estimated.
+  threshold: float 
+      The threshold on the skin color probability (between [0, 1]).
 
-    ``threshold`` (float [0-1]):
-      The threshold on the skin color probability.
+  bounding_boxes: list of :py:class: bob.ip.facedetect.BoundingBox
+    The face bounding boxes corresponding to the sequence.
 
-    ``bounding_boxes`` (list of BoundingBoxes):
-      The face bounding boxes corresponding to the sequence.
+  skin_frame: numpy.ndarray
+    The frame where the skin pixels have to be retrieved.
+    If not set, face_frame will be used.
 
-    ``skin_frame`` (numpy array):
-      The frame where the skin pixels have to be retrieved.
-      If not set, face_frame will be used.
+  plot: bool
+    Flag to plot the result of skin pixels detection
 
-    ``plot`` (boolean):
-      Flag to plot the result of skin pixels detection
-
-    **Returns**
-
-      ``skin_pixels`` (numpy array):
-        The RGB values of all detected skin colored pixels
+  Returns
+  -------
+  skin_pixels: numpy.ndarray
+    The RGB values of all detected skin colored pixels
+  
   """
   if skin_frame is None:
     skin_frame = face_frame
@@ -86,22 +68,20 @@ def get_skin_pixels(face_frame, index, skininit, threshold, bounding_boxes=None,
   return skin_pixels
 
 def get_eigen(skin_pixels):
-  """get_eigen(skin_pixels) -> eigenvalues, eigenvectors
-  
-    Build the C matrix, get eigenvalues and eigenvectors, sort them.
+  """build the C matrix, get eigenvalues and eigenvectors, sort them.
 
-    **Parameters**
-
-      ``skin_pixels`` (numpy array):
-        The RGB values of skin-colored pixels.
+  Parameters
+  ----------
+  skin_pixels: numpynd.array
+    The RGB values of skin-colored pixels.
         
-    **Returns**
+  Returns
+  -------
+  eigenvalues: numpy.ndarray
+    The eigenvalues of the correlation matrix
 
-      ``eigenvalues`` (numpy array):
-        The eigenvalues of the correlation matrix
-
-      ``eigenvectors`` (numpy array):
-        The (sorted) eigenvectors of the correlation matrix
+  eigenvectors: numpy.ndarray
+    The (sorted) eigenvectors of the correlation matrix
 
   """
   # build the correlation matrix
@@ -116,17 +96,15 @@ def get_eigen(skin_pixels):
   return eigenvalues, eigenvectors
 
 def plot_eigenvectors(skin_pixels, eigenvectors):
-  """plot_eigenvectors(skin_pixels, eigenvectors, counter)
-  
-    Plots skin pixel cluster and eignevectors in the RGB space.
+  """plots skin pixel cluster and eignevectors in the RGB space.
 
-    **Parameters**
+  Parameters
+  ----------
+  skin_pixels: numpynd.array
+    The RGB values of skin-colored pixels.
+  eigenvectors: numpy.ndarray
+    The eigenvectors of the correlation matrix
 
-      ``skin_pixels`` (numpy array):
-        The RGB values of skin-colored pixels.
-
-      ``eigenvectors`` (numpy array):
-        The eigenvectors of the correlation matrix. 
   """
   origin = numpy.array([0, 0, 0])
   import matplotlib.pyplot as plt
@@ -140,31 +118,26 @@ def plot_eigenvectors(skin_pixels, eigenvectors):
   plt.show()
 
 def build_P(counter, temporal_stride, eigenvectors, eigenvalues, plot=False):
-  """build_P(counter, temporal_stride, eigenvectors, eigenvalues, plot=False) -> p
+  """builds P
+
+  Parameters
+  ----------
+  counter: int
+    The frame index
+  temporal_stride: int
+    The temporal stride to use
+  eigenvectors: numpy.ndarray
+    The eigenvectors of the c matrix (for all frames up to counter). 
+  eigenvalues: numpy.ndarray
+    The eigenvalues of the c matrix (for all frames up to counter).
+  plot: bool
+    If you want something to be plotted
+
+  Returns
+  -------
+  p: numpy.ndarray
+    The p signal to add to the pulse.
   
-    Builds P
-
-    **Parameters**
-  
-      ``counter`` (int):
-        The frame index
-
-      ``temporal_stride`` (int):
-        The temporal stride to use
-
-      ``eigenvectors`` (numpy array):
-        The eigenvectors of the c matrix (for all frames up to counter). 
-    
-      ``eigenvalues`` (numpy array):
-        The eigenvalues of the c matrix (for all frames up to counter).
-
-      ``plot`` (boolean):
-        If you want something to be plotted
-
-    **Returns**
-
-      ``p`` (numpy array):
-        The p signal to add to the pulse.
   """
   tau = counter - temporal_stride
          
